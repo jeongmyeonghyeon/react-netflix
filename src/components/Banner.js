554be48c9ef5007f4e336761a30e48d162de09b9
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-25 00:51:22 +0900

    movieDetail 데이터 가져오기 전 처리

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -23,37 +23,37 @@ function Banner() {
       params: { append_to_response: "videos" },
     });
 
-    console.log("movieDetail", movieDetail);
+    // console.log("movieDetail", movieDetail);
     setMovie(movieDetail);
   };
 
-  // console.log(movie);
-
-  if (!movie) {
+  if (movie.length === 0) {
     return <div>Loading...</div>;
   }
 
   return (
-    <section
-      className="banner"
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
-        backgroundPosition: "top center",
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="banner__contents">
-        <h1 className="banner__title">
-          {movie.title || movie.name || movie.original_name}
-        </h1>
-        <div className="banner__buttons">
-          <div className="banner__button play">Play</div>
-          <div className="banner__button info">More Information</div>
+    movie && (
+      <section
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie.title || movie.name || movie.original_name}
+          </h1>
+          <div className="banner__buttons">
+            <div className="banner__button play">Play</div>
+            <div className="banner__button info">More Information</div>
+          </div>
+          <p className="banner__description">{movie.overview}</p>
         </div>
-        <p className="banner__description">{movie.overview}</p>
-      </div>
-      <div className="banner--fadeBottom"></div>
-    </section>
+        <div className="banner--fadeBottom"></div>
+      </section>
+    )
   );
 }
 
